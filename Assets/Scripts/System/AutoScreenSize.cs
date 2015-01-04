0294c286ee74e6c2881a836c21fafe72655710ab
ShinUnuma <gagagagiguss@gmail.com>
2015-01-04 15:59:16 +0900

    2Dと3Dのカメラ追加

---
+++ Assets/Scripts/System/AutoScreenSize.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoScreenSize : Singleton<AutoScreenSize>
+{
+	public float ScreenAspect { get{ return screentAspect; } }
+	private float screentAspect = 0.0f;
+	private Camera cameraMain;
+
+
+	public AutoScreenSize()
+	{
+		cameraMain = Camera.main;
+		float screenWidth = Screen.width;
+		float screenHeight = Screen.height;
+		screentAspect = screenWidth / screenHeight;
+	}
+}
---
+++ Assets/Scripts/System/Singleton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> where T : class, new()
+{
+	static readonly T instance = new T();
+
+	static Singleton() {  }
+	
+	protected Singleton() { }
+	
+	public static T Instance { get { return instance; } }
+}
